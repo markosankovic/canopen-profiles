37fc22fa110789e935d4f91ff20b0d606a1cf41e
Marko Sanković <sankovic.marko@gmail.com>
2017-02-26 14:37:34 +0100

    Add profile and predefined units

--- src/app/units/unit-list.component.ts
+++ src/app/units/unit-list.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as _ from 'lodash';
 
-import { Units } from './unit';
-import { Unit } from './unit';
-import { Prefix } from './unit';
+import { Unit, Units, UnitValue, Prefix } from './unit';
+
 
 @Component({
   templateUrl: './unit-list.component.html',
@@ -13,14 +12,7 @@ import { Prefix } from './unit';
 export class UnitListComponent {
 
   units: Units;
-  prefix: Prefix;
-
-  selectedPrefix: Prefix;
-  selectedNumerator: Unit;
-  selectedDenominator: Unit;
-
-  unitValue: string = '#x00000000';
-  unitAddon: string;
+  unitValue: UnitValue = new UnitValue(null, null, null);
 
   constructor(
     private route: ActivatedRoute,
@@ -31,42 +23,5 @@ export class UnitListComponent {
     this.route.data.subscribe((data: { units: Units }) => {
       this.units = data.units;
     });
-  }
-
-  onSelectPrefix(prefix: Prefix) {
-    this.selectedPrefix = prefix;
-    this.updateUnitValueAndAddon();
-  }
-
-  onSelectNumerator(numerator: Unit) {
-    this.selectedNumerator = numerator;
-    this.updateUnitValueAndAddon();
-  }
-
-  onSelectDenominator(denominator: Unit) {
-    this.selectedDenominator = denominator;
-    this.updateUnitValueAndAddon();
-  }
-
-  updateUnitValueAndAddon() {
-    let unitValue = "#x00000000";
-    let unitAddon = '';
-    if (this.selectedPrefix) {
-      unitValue = unitValue.substr(0, 2) + _.padStart(this.selectedPrefix.notationIndexDecimal.toString(16).toUpperCase(), 2, '0') + unitValue.substr(4);
-      unitAddon += this.selectedPrefix.symbolString;
-    }
-    if (this.selectedNumerator) {
-      unitValue = unitValue.substr(0, 4) + _.padStart(this.selectedNumerator.notationIndexDecimal.toString(16).toUpperCase(), 2, '0') + unitValue.substr(6);
-      unitAddon += this.selectedNumerator.symbolString;
-    }
-    if (this.selectedDenominator) {
-      unitValue = unitValue.substr(0, 6) + _.padStart(this.selectedDenominator.notationIndexDecimal.toString(16).toUpperCase(), 2, '0') + unitValue.substr(8);
-      if (this.selectedNumerator && this.selectedNumerator.symbolString && this.selectedDenominator.symbolString) {
-        unitAddon += '/';
-      }
-      unitAddon += this.selectedDenominator.symbolString;
-    }
-    this.unitValue = unitValue;
-    this.unitAddon = unitAddon;
   }
 }
--- src/app/units/unit.ts
+++ src/app/units/unit.ts
@@ -1,19 +1,38 @@
-export class Units {
+import * as _ from 'lodash';
 
-  constructor(public unitTypeAreas: Array<UnitTypeArea>, public prefixes: Array<Prefix>) { }
+export class Units {
+  constructor(public unitTypeAreas: Array<UnitTypeArea>, public prefixes: Array<Prefix>, public predefinedUnits: Array<UnitValue>) { }
+
+  predefinedUnitToUnitValue(predefinedUnit: string): UnitValue {
+    let unitValue = new UnitValue(null, null, null);
+    if (this.prefixes) {
+      let notationIndex = Number.parseInt(predefinedUnit.substr(2, 2), 16);
+      unitValue.prefix = this.prefixes.find(prefix => Number.parseInt(prefix.notationIndex.toString(), 16) === notationIndex);
+    }
+    if (this.unitTypeAreas) {
+      let units = _.chain(this.unitTypeAreas).map('units').flatten().value() as Array<Unit>;
+      let numeratorNotationIndex = Number.parseInt(predefinedUnit.substr(4, 2), 16);
+      unitValue.numerator = units.find(unit => Number.parseInt(unit.notationIndex.toString(), 16) === numeratorNotationIndex);
+      let denominatorNotationIndex = Number.parseInt(predefinedUnit.substr(6, 2), 16);
+      unitValue.denominator = units.find(unit => Number.parseInt(unit.notationIndex.toString(), 16) === denominatorNotationIndex);
+    }
+    return unitValue;
+  }
 
   static fromObject(src: Object): Units {
     let unitTypeAreas = new Array<UnitTypeArea>();
     src['unitTypeAreas'].forEach(unitTypeArea => unitTypeAreas.push(UnitTypeArea.fromObject(unitTypeArea)));
     let prefixes = new Array<Prefix>();
     src['prefixes'].forEach(prefix => prefixes.push(Prefix.fromObject(prefix)));
-    let units = new Units(unitTypeAreas, prefixes);
+    let units = new Units(unitTypeAreas, prefixes, null);
+    let predefinedUnits = new Array<UnitValue>();
+    src['predefinedUnits'].forEach(predefinedUnit => predefinedUnits.push(units.predefinedUnitToUnitValue(predefinedUnit)));
+    units.predefinedUnits = predefinedUnits;
     return units;
   }
 }
 
 export class UnitTypeArea {
-
   constructor(public index: string | Array<string>, public name: string, public units: Array<Unit>) { }
 
   static fromObject(src: Object): UnitTypeArea {
@@ -25,7 +44,6 @@ export class UnitTypeArea {
 }
 
 export class Unit {
-
   constructor(public name: string, public symbol: string, public notationIndex: string | Array<string>, public index: string | Array<string>, public description: string) { }
 
   static fromObject(src: Object): Unit {
@@ -42,7 +60,6 @@ export class Unit {
 }
 
 export class Prefix {
-
   constructor(public prefix: string, public symbol: string, public factor: string, public notationIndex: string | Array<string>) { }
 
   static fromObject(src: Object): Prefix {
@@ -55,5 +72,40 @@ export class Prefix {
 
   get notationIndexDecimal(): number {
     return typeof this.notationIndex === 'string' ? Number.parseInt(this.notationIndex, 16) : 0;
+  }
+}
+
+export class UnitValue {
+  constructor(public prefix: Prefix, public numerator: Unit, public denominator: Unit) { }
+
+  get index(): string {
+    let index = "#x00000000";
+    if (this.prefix) {
+      index = index.substr(0, 2) + _.padStart(this.prefix.notationIndexDecimal.toString(16).toUpperCase(), 2, '0') + index.substr(4);
+    }
+    if (this.numerator) {
+      index = index.substr(0, 4) + _.padStart(this.numerator.notationIndexDecimal.toString(16).toUpperCase(), 2, '0') + index.substr(6);
+    }
+    if (this.denominator) {
+      index = index.substr(0, 6) + _.padStart(this.denominator.notationIndexDecimal.toString(16).toUpperCase(), 2, '0') + index.substr(8);
+    }
+    return index;
+  }
+
+  get symbol(): string {
+    let symbol = '';
+    if (this.prefix) {
+      symbol += this.prefix.symbolString;
+    }
+    if (this.numerator) {
+      symbol += this.numerator.symbolString;
+    }
+    if (this.denominator) {
+      if (this.numerator && this.numerator.symbolString && this.denominator.symbolString) {
+        symbol += '/';
+      }
+      symbol += this.denominator.symbolString;
+    }
+    return symbol;
   }
 }
